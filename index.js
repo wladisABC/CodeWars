e03d15c0480ce1586ac46a03f7a94751acbb2e28
Wlad <grishaw@mail.ru>
2025-04-14 16:49:40 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const spinWords= (string) => {
+    const arr = string.split(' ');
+    const result = arr.map(word => {
+        return word.length >= 5 ? word.split('').reverse().join('') : word;
+    });
+    return result.join(' ');
+}
+
+spinWords('Just kidding there is still one more')
